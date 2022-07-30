f8871c95a347fa47505a9a908f947855dcba0ed9
Julien C <hoopsinet@hotmail.fr>
2022-07-30 16:41:43 +0200

    ajouter un produit au panier

--- front/js/product.js
+++ front/js/product.js
@@ -2,29 +2,44 @@
 // fonction qui recup tous les kanaps
 function getkanaps() {
     const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get('id');
-    return fetch (`http://localhost:3000/api/products/${id}`)
+    const kanapId = urlParams.get('id');
+    return fetch (`http://localhost:3000/api/products/${kanapId}`)
+   
     .then (function (response) {
         return response.json()
     })
-    .then (function (kanaps) {
-        displaykanaps(kanaps)
+    .then (function (kanap) {
+        displaykanap(kanap)
     })
     .catch (function (error) {
         alert (error)
     })
 }
 // fonction qui permet d'afficher les kanaps
-function displaykanaps(kanaps){
-    for (let kanap of kanaps) {
-        document.querySelector(`#items`).innerHTML += ` <a href="./product.html?id=${kanap._id}">
-    <article>
-    <img src="${kanap.imageUrl}" alt="${kanap.name}">
-    <h3 class="productName">${kanap.name}</h3>
-    <p class="productDescription">${kanap.description} </p> </article>
-    </a>` 
+    function displaykanap(kanap){
+        const image = document.querySelector(".item__img");
+        const title = document.querySelector("#title");
+        const price = document.querySelector("#price");
+        const description = document.querySelector("#description");
+        const colors = document.querySelector("#colors");
+       
+        let img = document.createElement("img");
+        img.src = kanap.imageUrl
+        
+        image.appendChild(img);
+        title.textContent = kanap.name
+        price.textContent = kanap.price
+        description.textContent = kanap.description
+        
+
+        kanap.colors.forEach(color=> {
+            let option = document.createElement("option");
+            option.value = color;
+            option.textContent += color
+            colors.appendChild(option);
+          });
+
+
     }     
-}
-getkanaps()
-
-
+getkanaps();
+// displaykanap();
