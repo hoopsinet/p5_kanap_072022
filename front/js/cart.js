307be6693c055719a2a34c14e93e1934ea56c9bb
Julien C <hoopsinet@hotmail.fr>
2022-08-06 16:41:29 +0200

    Changer la quantité dans le panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,6 @@
 getKanaps();
-getCart();
-// document.querySelector('.deleteItem').addEventListener('click', removeFromCart);
+getCart();    
+changeQuantity();
 getNumberProduct();
 getTotalPrice();
 
@@ -60,26 +60,57 @@ function getCart() {
             }
         
     
-
+            let deleteItem = document.querySelectorAll('.deleteItem');
+            for(let i = 0 ; i < deleteItem.length ; i++) {
+                deleteItem[i].addEventListener('click' , () => removeFromCart(i));
+            }
 }
 
-// //  REtirer du cart
-// function removeFromCart(product){
-//     let cart = getCart();
-//     cart = cart.filter(p => p.id != product.id);
-//     localStorage.setItem('cart'), JSON.stringify(cart)
-//     displayCart(kanap)
-// }
 
-// // Calculer le total de produit dans le cart
-//     function getNumberProduct() {
-//     let cart = getCart()
-//     let number = 0;
-//     for (let product of cart) {
-//         number += product.quantity;
-//     }
-//     return number;
-//     }
+//  REtirer du cart
+async function removeFromCart(product) {
+    let cart = getCart();
+    cart.splice(product , 1);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    document.location.reload();  
+}
+async function changeQuantity(product, quantity) {
+    //je récupère le contenu du localStorage (le panier =cart)
+    let cart = getCart();
+
+    // (i = index j'initialise à 0 et le travail se fait temps qu'il y a des produits dans le panier)
+    for(let i = 0 ; i < cart.length ; i++) {
+        // un produit e, particulier dans le panier, i étant sa place dans le panier = son index
+        let product = cart[i];
+        // la quantité du produit dans le panier (ou localStorage)
+        let quantity = product.quantity;
+        // récupère le "bouton" selon son index qui permet à l'utilisateur de changer la quantité
+        let quantityChange = document.querySelectorAll('.itemQuantity')[i];
+        //la valeur qui a pu (ou non) être changée
+        let userQuantityChange = quantityChange.value;
+        // retrouver dans le panier(localStorage) l'id correspondant au produit dont la quantité est modifiable
+        let foundProduct = cart.find(p => p.id == product.id);
+
+        if(foundProduct) {
+            quantity = userQuantityChange;
+            if(userQuantityChange <= 0) {
+                removeFromCart(foundProduct);
+            }
+                localStorage.setItem('cart', JSON.stringify(cart));
+                document.location.reload();  
+        }
+    }
+}
+
+// Calculer le total de produit dans le cart
+    function getNumberProduct() {
+    let cart = getCart()
+    let number = 0;
+    for (let product of cart) {
+        number += product.quantity;
+    }
+    return number;
+    }
 // // Calculer le prix total du cart
 //     function getTotalPrice() {
 //     let cart = getCart();
@@ -90,19 +121,17 @@ function getCart() {
 //     }
 //     return number;
 //     }
-//     // Changer la quantité du cart
-// function changeQuantity(product,quantity) {
+    // Changer la quantité du cart
+
+
+//  REtirer du cart
+// function removeFromCart(product){
 //     let cart = getCart();
-//     let foundProduct = cart.find(p => p.id == product.id);
-//     if(foundProduct != undefined) {
-//         foundProduct.quantity += quantity;
-//         if(foundProduct.quantity <= 0) {
-//             removeFromCart(foundProduct);
-//         }else{
-//             saveCart(cart)
-//         }
-//     }
+//     cart = cart.filter(p => p.id != product.id);
+//     localStorage.setItem('cart'), JSON.stringify(cart)
+//     displayCart(kanap)
 // }
+
 
 // ajoutez un eventlistener pour le formulaire.
 //  si il est bien rempli faire appel à l'api
