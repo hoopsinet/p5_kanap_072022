733bdbf623bbb22b91a43e4aa622a5b4816f9fa4
Julien C <hoopsinet@hotmail.fr>
2022-08-13 16:13:44 +0200

    verification du formulaire (regex)

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,7 +2,6 @@ getKanaps();
 getCart();
 changeQuantity();
 getNumberProduct();
-getTotalPrice();
 
 //  On récupére un " item "
 function getCart() {
@@ -76,7 +75,7 @@ function displayCart(kanaps) {
 
 let itemQuantity = document.querySelectorAll(".itemQuantity");
             for(let i = 0 ; i < itemQuantity.length ; i++) {
-                itemQuantity[i].addEventListener('click' , () => changeQuantity(i));
+                itemQuantity[i].addEventListener('change' , () => changeQuantity(i));
             }
 }
 }
@@ -108,24 +107,180 @@ function changeQuantity() {
           quantity = parseInt(quantity[i].value);
           cart[i].quantity = quantity;
           localStorage.setItem('cart', JSON.stringify(cart)); 
-      }
+         }
 }
 
-// Calculer le prix total du cart
-    function getTotalPrice() {
-    let cart = getCart();
-    let total = 0;
-    for (let product of cart) {
-        total += product.quantity * product.price;
-        localStorage.setItem('cart', JSON.stringify(cart))
-    }
-    return total;
-    }
-// Changer la quantité du cart
+
+// ************************  FORMULAIRE  *************************
 
 
 // ajoutez un eventlistener pour le formulaire.
 //  si il est bien rempli faire appel à l'api
 
 
-//chaine de caractére (spéciaux ou non) obligatoire un @ >> Domain (hotmail/gmail)  .  3caractére (com/fr)  reggex
+// chaine de caractére (spéciaux ou non) obligatoire un @ >> Domain (hotmail/gmail)  .  3caractére (com/fr)  reggex
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const address = document.getElementById("address");
+const city = document.getElementById("city");
+const email = document.getElementById("email");
+
+let valueFirstName, valueLastName, valueAddress, valueCity, valueEmail;
+//************  Configuration pour l'input PRENOM ************
+firstName.addEventListener("input" , function (e) {
+// ********** e.target = à FirstName.value *********
+  valueFirstName;
+  if (e.target.value.length == 0) {
+    console.log("rien");
+    firstNameErrorMsg.innerHTML = "";
+    valueFirstName = null;
+    console.log(valueFirstName);
+  }   else if (e.target.value.length < 3 || e.target.value.length > 25){
+    firstNameErrorMsg.innerHTML = "le prénom doit contenir entre 3 à 25 caractéres";
+      valueFirstName = null
+      console.log("pas assez ou trop de lettres");
+}
+     if (e.target.value.match(/^[a-z A-Z éèê-]{3,25}$/)) {
+    firstNameErrorMsg.innerHTML = "";
+    valueFirstName = e.target.value;
+    console.log("ça marche");
+    console.log(valueFirstName);
+  }
+  if (
+    // Different  " ! "
+    !e.target.value.match(/^[a-z A-Z éèê-]{3,25}$/) && 
+    e.target.value.length > 3 && 
+    e.target.value.length < 25
+    ) {
+      firstNameErrorMsg.innerHTML = "le prénom ne doit pas contenir de caractère spécial ou de chiffre"
+      valueFirstName = null;
+      console.log("Ne correspond pas à nos attentes");
+    }
+
+});
+
+lastName.addEventListener("input" , function (e) {
+  // ********** e.target = à LastName.value *********
+    valueLastName;
+    if (e.target.value.length == 0) {
+      console.log("rien");
+      errorLastName.innerHTML = "";
+      valueLastName = null;
+      console.log(valueLastName);
+    }   else if (e.target.value.length < 3 || e.target.value.length > 25){
+      lastNameErrorMsg.innerHTML = "le nom doit contenir entre 3 à 25 caractéres";
+        valueLastName = null
+        console.log("pas assez ou trop de lettres");
+  }
+       if (e.target.value.match(/^[a-z A-Z éèê-]{3,25}$/)) {
+      lastNameErrorMsg.innerHTML = "";
+      valueLastName = e.target.value;
+      console.log("ça marche");
+      console.log(valueLastName);
+    }
+    if (
+      // Different  " ! "
+      !e.target.value.match(/^[a-z A-Z éèê-]{3,25}$/) && 
+      e.target.value.length > 3 && 
+      e.target.value.length < 25
+      ) {
+        lastNameErrorMsg.innerHTML = "le nom ne doit pas contenir de caractère spéciaux ou de chiffre"
+        valueLastName = null;
+        console.log("Ne correspond pas à nos attentes");
+      }
+  
+  });
+  
+  address.addEventListener("input" , function (e) {
+    // ********** e.target = à address value *********
+      valueAddress;
+      if (e.target.value.length == 0) {
+        console.log("rien");
+        errorAddress.innerHTML = "";
+        valueAddress = null;
+        console.log(valueAddress);
+      }   else if (e.target.value.length < 3 || e.target.value.length > 35){
+        addressErrorMsg.innerHTML = "l'adresse doit contenir entre 3 à 35 caractéres";
+          valueAddress = null
+          console.log("pas assez ou trop de lettres");
+    }
+         if (e.target.value.match(/^[0-9]{1,3} [a-z A-Z éèê]{3,25}$/)) {
+        addressErrorMsg.innerHTML = "";
+        valueAddress = e.target.value;
+        console.log("ça marche");
+        console.log(valueAddress);
+      }
+      if (
+        // Different  " ! "
+        !e.target.value.match(/^[0-9]{1,3} [a-z A-Z éèê]{3,25}$/) && 
+        e.target.value.length > 3 && 
+        e.target.value.length < 35
+        ) {
+          addressErrorMsg.innerHTML = "l'adresse commence par des chiffres ensuite par des lettres, pas de caractére spéciaux"
+          valueAddress = null;
+          console.log("Ne correspond pas à nos attentes");
+        }
+    
+    });
+    city.addEventListener("input" , function (e) {
+      // ********** e.target = à city.value *********
+        valueCity;
+        if (e.target.value.length == 0) {
+          console.log("rien");
+          cityErrorMsg.innerHTML = "";
+          valueCity = null;
+          console.log(valueCity);
+        }   else if (e.target.value.length < 3 || e.target.value.length > 25){
+          cityErrorMsg.innerHTML = "la ville doit contenir entre 3 à 25 caractéres";
+            valueCity = null
+            console.log("pas assez ou trop de lettres");
+      }
+           if (e.target.value.match(/^[\s] [a-z A-Z éèê-]{3,25}$/)) {
+          cityErrorMsg.innerHTML = "";
+          valueCity = e.target.value;
+          console.log("ça marche");
+          console.log(valueCity);
+        }
+        if (
+          // Different  " ! "
+          !e.target.value.match(/^[\s] [a-z A-Z éèê-]{3,25}$/) && 
+          e.target.value.length > 3 && 
+          e.target.value.length < 25
+          ) {
+            cityErrorMsg.innerHTML = "le nom ne doit pas contenir de caractère spéciaux ou de chiffre"
+            valueCity = null;
+            console.log("Ne correspond pas à nos attentes");
+          }
+      
+      });
+
+      email.addEventListener("input" , (e) => {
+        if (e.target.value.length == 0) {
+          emailErrorMsg.innerHTML = ""
+          valueEmail = null;
+          console.log(valueEmail);
+        }
+        // ecriture avec @ avec . [Fr.com.etc...]
+        else if (e.target.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+          emailErrorMsg.innerHTML = ""
+          valueEmail = e.target.value;
+          console.log(valueEmail);
+        }
+        // 1 seule conditions pour toutes les erreurs
+        if (!e.target.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/) &&  e.target.value.length == 0) {
+          emailErrorMsg.innerHTML = "Email incorrect ex: hoopsi@gmail.com"
+          valueEmail = null;
+        }
+      });
+
+
+/// const button = document.querySelector("#order");
+// button.addEventListener('click', () => validation());
+
+// firstName.addEventListener("input", function (e)) {
+
+// }
+// // A valider la commande donc envoyer à l'api les données
+// // function validation() {
+  
+// // }
