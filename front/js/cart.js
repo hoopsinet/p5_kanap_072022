2be51f168b4b61d0879ff9d9eac5bba5431d08af
Julien C <hoopsinet@hotmail.fr>
2022-08-20 14:12:13 +0200

    validation de la commande

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,7 +1,9 @@
 getKanaps();
 getCart();
-changeQuantity();
-getNumberProduct();
+const sendOrder = document.querySelector("#order");
+sendOrder.addEventListener('click', order); 
+
+
 
 //  On récupére un " item "
 function getCart() {
@@ -30,16 +32,13 @@ function displayCart(kanaps) {
   document.querySelector(`#cart__items`).innerHTML = ``;
   if (cart) {
     // Initialisation d'un tableau dans lequel je retrouve les prix totaux des produits. (quantité * prix)
-    let prices = [];
+    let prices = [0];
     // pour chaque produit du cart
-    let quantity = 0;
-    for (let i = 0; i < cart.length; i++) {
+     for (let i = 0; i < cart.length; i++) {
       let product = cart[i];
       let kanap = kanaps.filter((kanap) => kanap._id == product.id)[0];
-
-      document.querySelector(
-        `#cart__items`
-      ).innerHTML += ` <article class="cart__item" data-id="${product.id}" data-color="${product.color}">
+      
+      document.querySelector(`#cart__items`).innerHTML += ` <article class="cart__item" data-id="${product.id}" data-color="${product.color}">
                                                                                 <div class="cart__item__img">
                                                                                 <img src="${kanap.imageUrl}" alt="Photographie d'un canapé">
                                                                                 </div>
@@ -67,17 +66,18 @@ function displayCart(kanaps) {
     // Methode reduce() pour calculer le total des valeurs dans le tableau [prices]
     let total = (accumulator, current) => accumulator + current;
             let totalPrices = prices.reduce(total);
-            document.querySelector("#totalPrice").textContent += totalPrices
+            document.querySelector("#totalPrice").textContent = totalPrices
   }
   let deleteItem = document.querySelectorAll(".deleteItem");
   for (let i = 0; i < deleteItem.length; i++) {
     deleteItem[i].addEventListener("click", () => removeFromCart(i));
-
-let itemQuantity = document.querySelectorAll(".itemQuantity");
-            for(let i = 0 ; i < itemQuantity.length ; i++) {
-                itemQuantity[i].addEventListener('change' , () => changeQuantity(i));
-            }
 }
+
+// Afficher le total d'article dans le panier
+let totalQuantity = getNumberProduct();
+document.querySelector("#totalQuantity").textContent = totalQuantity;
+
+changeQuantity();
 }
 //  REtirer du cart
 async function removeFromCart(product) {
@@ -104,12 +104,17 @@ function changeQuantity() {
   let cart = getCart();
   let quantity = document.querySelectorAll(".itemQuantity")
       for(let i = 0 ; i < quantity.length ; i++) {
-          quantity = parseInt(quantity[i].value);
-          cart[i].quantity = quantity;
-          localStorage.setItem('cart', JSON.stringify(cart)); 
-         }
+        quantity[i].addEventListener('change', (e) => {
+          cart[i].quantity = parseInt(e.target.value);
+          localStorage.setItem('cart', JSON.stringify(cart));
+          if (cart[i].quantity <= 0) {
+            removeFromCart();
+            getKanaps()
+          }
+          getKanaps();
+        }) 
+      }
 }
-
 
 // ************************  FORMULAIRE  *************************
 
@@ -128,132 +133,65 @@ const email = document.getElementById("email");
 let valueFirstName, valueLastName, valueAddress, valueCity, valueEmail;
 //************  Configuration pour l'input PRENOM ************
 firstName.addEventListener("input" , function (e) {
-// ********** e.target = à FirstName.value *********
-  valueFirstName;
-  if (e.target.value.length == 0) {
-    console.log("rien");
-    firstNameErrorMsg.innerHTML = "";
-    valueFirstName = null;
-    console.log(valueFirstName);
-  }   else if (e.target.value.length < 3 || e.target.value.length > 25){
-    firstNameErrorMsg.innerHTML = "le prénom doit contenir entre 3 à 25 caractéres";
-      valueFirstName = null
-      console.log("pas assez ou trop de lettres");
-}
-     if (e.target.value.match(/^[a-z A-Z éèê-]{3,25}$/)) {
-    firstNameErrorMsg.innerHTML = "";
-    valueFirstName = e.target.value;
-    console.log("ça marche");
-    console.log(valueFirstName);
-  }
-  if (
-    // Different  " ! "
-    !e.target.value.match(/^[a-z A-Z éèê-]{3,25}$/) && 
-    e.target.value.length > 3 && 
-    e.target.value.length < 25
-    ) {
-      firstNameErrorMsg.innerHTML = "le prénom ne doit pas contenir de caractère spécial ou de chiffre"
-      valueFirstName = null;
-      console.log("Ne correspond pas à nos attentes");
-    }
-
-});
+  // ********** e.target = à FirstName.value *********
+      valueFirstName;
+      if (e.target.value.match(/^[a-z A-Z éèê-]{3,25}$/)) {
+          firstNameErrorMsg.innerHTML = "";
+          valueFirstName = e.target.value;
+          console.log("ça marche");
+          console.log(valueFirstName);
+      } else {
+          firstNameErrorMsg.innerHTML = "le prénom ne doit pas contenir de caractère spécial ou de chiffre";
+          valueFirstName = null
+          console.log("pas assez ou trop de lettres");
+      }
+  });
 
 lastName.addEventListener("input" , function (e) {
   // ********** e.target = à LastName.value *********
-    valueLastName;
-    if (e.target.value.length == 0) {
-      console.log("rien");
-      errorLastName.innerHTML = "";
-      valueLastName = null;
-      console.log(valueLastName);
-    }   else if (e.target.value.length < 3 || e.target.value.length > 25){
-      lastNameErrorMsg.innerHTML = "le nom doit contenir entre 3 à 25 caractéres";
-        valueLastName = null
-        console.log("pas assez ou trop de lettres");
-  }
-       if (e.target.value.match(/^[a-z A-Z éèê-]{3,25}$/)) {
+  valueLastName;
+  if (e.target.value.match(/^[a-z A-Z éèê-]{3,25}$/)) {
       lastNameErrorMsg.innerHTML = "";
       valueLastName = e.target.value;
       console.log("ça marche");
       console.log(valueLastName);
-    }
-    if (
-      // Different  " ! "
-      !e.target.value.match(/^[a-z A-Z éèê-]{3,25}$/) && 
-      e.target.value.length > 3 && 
-      e.target.value.length < 25
-      ) {
-        lastNameErrorMsg.innerHTML = "le nom ne doit pas contenir de caractère spéciaux ou de chiffre"
-        valueLastName = null;
-        console.log("Ne correspond pas à nos attentes");
-      }
-  
-  });
-  
+  } else {
+      lastNameErrorMsg.innerHTML = "le prénom ne doit pas contenir de caractère spécial ou de chiffre";
+      valueLastName = null
+      console.log("pas assez ou trop de lettres");
+  }
+});
+
   address.addEventListener("input" , function (e) {
     // ********** e.target = à address value *********
       valueAddress;
-      if (e.target.value.length == 0) {
-        console.log("rien");
-        errorAddress.innerHTML = "";
-        valueAddress = null;
-        console.log(valueAddress);
-      }   else if (e.target.value.length < 3 || e.target.value.length > 35){
-        addressErrorMsg.innerHTML = "l'adresse doit contenir entre 3 à 35 caractéres";
+      if (e.target.value.match(/^[0-9]{1,3} [a-z A-Z éèê]{3,25}$/)) {
+          addressErrorMsg.innerHTML = "";
+          valueAddress = e.target.value;
+          console.log("ça marche");
+          console.log(valueAddress);
+      } else {
+          addressErrorMsg.innerHTML = "l'adresse commence par des chiffres ensuite par des lettres, pas de caractére spéciaux";
           valueAddress = null
           console.log("pas assez ou trop de lettres");
-    }
-         if (e.target.value.match(/^[0-9]{1,3} [a-z A-Z éèê]{3,25}$/)) {
-        addressErrorMsg.innerHTML = "";
-        valueAddress = e.target.value;
-        console.log("ça marche");
-        console.log(valueAddress);
       }
-      if (
-        // Different  " ! "
-        !e.target.value.match(/^[0-9]{1,3} [a-z A-Z éèê]{3,25}$/) && 
-        e.target.value.length > 3 && 
-        e.target.value.length < 35
-        ) {
-          addressErrorMsg.innerHTML = "l'adresse commence par des chiffres ensuite par des lettres, pas de caractére spéciaux"
-          valueAddress = null;
-          console.log("Ne correspond pas à nos attentes");
-        }
-    
-    });
+  });
+
     city.addEventListener("input" , function (e) {
       // ********** e.target = à city.value *********
         valueCity;
-        if (e.target.value.length == 0) {
-          console.log("rien");
-          cityErrorMsg.innerHTML = "";
-          valueCity = null;
-          console.log(valueCity);
-        }   else if (e.target.value.length < 3 || e.target.value.length > 25){
-          cityErrorMsg.innerHTML = "la ville doit contenir entre 3 à 25 caractéres";
-            valueCity = null
-            console.log("pas assez ou trop de lettres");
-      }
-           if (e.target.value.match(/^[\s] [a-z A-Z éèê-]{3,25}$/)) {
+        if (e.target.value.match(/^[a-z A-Z éèê-]{3,25}$/)) {
           cityErrorMsg.innerHTML = "";
           valueCity = e.target.value;
           console.log("ça marche");
           console.log(valueCity);
-        }
-        if (
-          // Different  " ! "
-          !e.target.value.match(/^[\s] [a-z A-Z éèê-]{3,25}$/) && 
-          e.target.value.length > 3 && 
-          e.target.value.length < 25
-          ) {
-            cityErrorMsg.innerHTML = "le nom ne doit pas contenir de caractère spéciaux ou de chiffre"
-            valueCity = null;
-            console.log("Ne correspond pas à nos attentes");
-          }
-      
-      });
-
+      } else {
+          cityErrorMsg.innerHTML = "la ville doit contenir entre 3 à 25 caractéres";
+          valueCity = null
+          console.log("pas assez ou trop de lettres");
+      }
+  });
+         
       email.addEventListener("input" , (e) => {
         if (e.target.value.length == 0) {
           emailErrorMsg.innerHTML = ""
@@ -261,28 +199,50 @@ lastName.addEventListener("input" , function (e) {
           console.log(valueEmail);
         }
         // ecriture avec @ avec . [Fr.com.etc...]
-        else if (e.target.value.match(/^[a-z0-9.-]+@[a-z0-9.-]{2,}[.][a-z]{2,4}$/)) {
+        else if (e.target.value.match(/^[a-z0-9.-]+@[a-z0-9.-]{2,}[.][a-z]{2,3}$/)) {
           emailErrorMsg.innerHTML = ""
           valueEmail = e.target.value;
           console.log(valueEmail);
         }
         // 1 seule conditions pour toutes les erreurs
-        if (!e.target.value.match(/^[a-z0-9.-]+@[a-z0-9.-]{2,}[.][a-z]{2,4}$/) && 
-            !e.target.value.length == 0
-         ) {
+        else if (!e.target.value.match(/^[a-z0-9.-]+@[a-z0-9.-]{2,}[.][a-z]{2,3}$/) && !e.target.value.length == 0) {
           emailErrorMsg.innerHTML = "Email incorrect ex: hoopsi@gmail.com"
           valueEmail = null;
         }
       });
 
 
-/// const button = document.querySelector("#order");
-// button.addEventListener('click', () => validation());
 
-// firstName.addEventListener("input", function (e)) {
-
-// }
-// // A valider la commande donc envoyer à l'api les données
-// // function validation() {
+ function order() {
   
-// // }
+let products = getCart();
+
+const push = {
+  contact : {
+    firstName : valueFirstName,
+    lastName : valueLastName, 
+    address : valueAddress,
+    city : valueCity,
+    email : valueEmail
+    },
+  products
+};
+console.log(push)
+
+const request = fetch ( 'http://localhost:3000/order', {
+        method: 'POST',
+        headers: { 
+            'Content-Type':'application/json',
+            body: JSON.stringify(push),
+        }
+        
+    });
+    let response = await request.json();
+    window.location.replace('./confirmation.html?orderId=${id}')
+
+    // const orderId = await request.json();
+    // const id = await orderId;
+    
+
+  
+}
---
+++ front/js/confirmation.js
@@ -0,0 +1,10 @@
+getOrderId();
+//  on va chercher dans l'url l'orderId et on l'affiche de la page html attendu
+function getOrderId() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const orderId = urlParams.get('orderId');
+    document.querySelector('#orderId').textContent = orderId;
+
+ 
+}
+ 
